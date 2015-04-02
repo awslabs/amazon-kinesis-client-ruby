2bd20783c6296336804a43bd3564042dbe6c9fa1
Peak Xu <peak.xu@gmail.com>
2015-04-02 09:50:41 -0400

    Set includes for producer sample to AWS SDK v2 compatible

--- samples/sample_kcl_producer.rb
+++ samples/sample_kcl_producer.rb
@@ -1,19 +1,19 @@
 #! /usr/bin/env ruby
 #
 #  Copyright 2014 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# 
+#
 #  Licensed under the Amazon Software License (the "License").
 #  You may not use this file except in compliance with the License.
 #  A copy of the License is located at
-# 
+#
 #  http://aws.amazon.com/asl/
-# 
+#
 #  or in the "license" file accompanying this file. This file is distributed
 #  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
 #  express or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
 
-require 'aws/kinesis'
+require 'aws-sdk'
 require 'multi_json'
 require 'optparse'
 
@@ -39,8 +39,8 @@ class SampleProducer
     begin
       @kinesis.delete_stream(:stream_name => @stream_name)
       puts "Deleted stream #{@stream_name}"
-    rescue AWS::Kinesis::Errors::ResourceNotFoundException
-      # nothing to do 
+    rescue Aws::Kinesis::Errors::ResourceNotFoundException
+      # nothing to do
     end
   end
 
@@ -56,7 +56,7 @@ class SampleProducer
         fail "Stream #{@stream_name} has #{desc[:shards].size} shards, while requested number of shards is #{@shard_count}"
       end
       puts "Stream #{@stream_name} already exists with #{desc[:shards].size} shards"
-    rescue AWS::Kinesis::Errors::ResourceNotFoundException
+    rescue Aws::Kinesis::Errors::ResourceNotFoundException
       puts "Creating stream #{@stream_name} with #{@shard_count || 2} shards"
       @kinesis.create_stream(:stream_name => @stream_name,
                              :shard_count => @shard_count || 2)
@@ -142,7 +142,7 @@ if __FILE__ == $0
   # See http://docs.aws.amazon.com/AWSJavaSDK/latest/javadoc/com/amazonaws/auth/DefaultAWSCredentialsProviderChain.html
   kconfig = {}
   kconfig[:region] = aws_region  if aws_region
-  kinesis = AWS::Kinesis::Client.new(kconfig)
+  kinesis = Aws::Kinesis::Client.new(kconfig)
 
   producer = SampleProducer.new(kinesis, stream_name, sleep_between_puts, shard_count)
   producer.run(timeout)
