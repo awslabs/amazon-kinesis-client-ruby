c8c3bb2050d79810aa649e7ae255032fb4a281a0
skye rogers <skyero@amazon.com>
2025-11-18 14:17:39 -0800

    kcl github workflows

--- .github/scripts/manipulate_properties.sh
+++ .github/scripts/manipulate_properties.sh
@@ -10,20 +10,16 @@ if [[ "$RUNNER_OS" == "macOS" ]]; then
   grep -v "idleTimeBetweenReadsInMillis" samples/sample.properties > samples/temp.properties
   echo "idleTimeBetweenReadsInMillis = 250" >> samples/temp.properties
   mv samples/temp.properties samples/sample.properties
-elif [[ "$RUNNER_OS" == "Linux" ]]; then
+elif [[ "$RUNNER_OS" == "Linux" || "$RUNNER_OS" == "Windows" ]]; then
   sed -i "s/kclrbsample/$STREAM_NAME/g" samples/sample.properties
   sed -i "s/RubyKCLSample/$APP_NAME/g" samples/sample.properties
   sed -i 's/us-east-5/us-east-1/g' samples/sample.properties
   sed -i "/idleTimeBetweenReadsInMillis/c\idleTimeBetweenReadsInMillis = 250" samples/sample.properties
-elif [[ "$RUNNER_OS" == "Windows" ]]; then
-  sed -i "s/kclrbsample/$STREAM_NAME/g" samples/sample.properties
-  sed -i "s/RubyKCLSample/$APP_NAME/g" samples/sample.properties
-  sed -i 's/us-east-5/us-east-1/g' samples/sample.properties
-  sed -i "/idleTimeBetweenReadsInMillis/c\idleTimeBetweenReadsInMillis = 250" samples/sample.properties
-
-  echo '@echo off' > samples/run_script.bat
-  echo 'ruby %~dp0\sample_kcl.rb %*' >> samples/run_script.bat
-  sed -i 's/executableName = sample_kcl.rb/executableName = run_script.bat/' samples/sample.properties
+  if [[ "$RUNNER_OS" == "Windows" ]]; then
+    echo '@echo off' > samples/run_script.bat
+    echo 'ruby %~dp0\sample_kcl.rb %*' >> samples/run_script.bat
+    sed -i 's/executableName = sample_kcl.rb/executableName = run_script.bat/' samples/sample.properties
+  fi
 else
   echo "Unknown OS: $RUNNER_OS"
   exit 1
