aa2fc4779cf8b07f17a2df8587f07e754413083f
skye rogers <skyero@amazon.com>
2025-12-03 11:48:00 -0800

    robust clean up strategy and added dedicated sample.properties file

--- .github/scripts/manipulate_properties.sh
+++ .github/scripts/manipulate_properties.sh
@@ -4,25 +4,19 @@ set -e
 # Manipulate sample.properties file that the KCL application pulls properties from (ex: streamName, applicationName)
 # Depending on the OS, different properties need to be changed
 if [[ "$RUNNER_OS" == "macOS" ]]; then
-  sed -i "" "s/kclrbsample/$STREAM_NAME/g" samples/sample.properties
-  sed -i "" "s/RubyKCLSample/$APP_NAME/g" samples/sample.properties
-  sed -i "" 's/us-east-5/us-east-1/g' samples/sample.properties
-  grep -v "idleTimeBetweenReadsInMillis" samples/sample.properties > samples/temp.properties
-  echo "idleTimeBetweenReadsInMillis = 250" >> samples/temp.properties
-  mv samples/temp.properties samples/sample.properties
+  sed -i "" "s/kclrbsample/$STREAM_NAME/g" .github/resources/sample.properties
+  sed -i "" "s/RubyKCLSample/$APP_NAME/g" .github/resources/sample.properties
 elif [[ "$RUNNER_OS" == "Linux" || "$RUNNER_OS" == "Windows" ]]; then
-  sed -i "s/kclrbsample/$STREAM_NAME/g" samples/sample.properties
-  sed -i "s/RubyKCLSample/$APP_NAME/g" samples/sample.properties
-  sed -i 's/us-east-5/us-east-1/g' samples/sample.properties
-  sed -i "/idleTimeBetweenReadsInMillis/c\idleTimeBetweenReadsInMillis = 250" samples/sample.properties
+  sed -i "s/kclrbsample/$STREAM_NAME/g" .github/resources/sample.properties
+  sed -i "s/RubyKCLSample/$APP_NAME/g" .github/resources/sample.properties
   if [[ "$RUNNER_OS" == "Windows" ]]; then
     echo '@echo off' > samples/run_script.bat
-    echo 'ruby %~dp0\sample_kcl.rb %*' >> samples/run_script.bat
-    sed -i 's/executableName = sample_kcl.rb/executableName = run_script.bat/' samples/sample.properties
+    echo 'ruby %~dp0sample_kcl.rb %*' >> samples/run_script.bat
+    sed -i 's/executableName = sample_kcl.rb/executableName = run_script.bat/' .github/resources/sample.properties
   fi
 else
   echo "Unknown OS: $RUNNER_OS"
   exit 1
 fi
 
-cat samples/sample.properties
+cat .github/resources/sample.properties
--- .github/scripts/start_kcl.sh
+++ .github/scripts/start_kcl.sh
@@ -4,18 +4,11 @@ set -o pipefail
 
 chmod +x samples/sample_kcl.rb
 
-# Get records from stream to verify they exist before continuing
-SHARD_ITERATOR=$(aws kinesis get-shard-iterator --stream-name $STREAM_NAME --shard-id shardId-000000000000 --shard-iterator-type TRIM_HORIZON --query 'ShardIterator' --output text)
-INITIAL_RECORDS=$(aws kinesis get-records --shard-iterator $SHARD_ITERATOR)
-RECORD_COUNT_BEFORE=$(echo $INITIAL_RECORDS | jq '.Records | length')
-
-echo "Found $RECORD_COUNT_BEFORE records in stream before KCL start"
-
 if [[ "$RUNNER_OS" == "macOS" ]]; then
   brew install coreutils
-  (cd samples && gtimeout $RUN_TIME_SECONDS rake run properties_file=sample.properties 2>&1 | tee ../kcl_output.log) || [ $? -eq 124 ]
+  (cd samples && gtimeout $RUN_TIME_SECONDS rake run properties_file=../.github/resources/sample.properties 2>&1 | tee ../kcl_output.log) || [ $? -eq 124 ]
 elif [[ "$RUNNER_OS" == "Linux" || "$RUNNER_OS" == "Windows" ]]; then
-  (cd samples && timeout $RUN_TIME_SECONDS rake run properties_file=sample.properties 2>&1 | tee ../kcl_output.log) || [ $? -eq 124 ]
+  (cd samples && timeout $RUN_TIME_SECONDS rake run properties_file=../.github/resources/sample.properties 2>&1 | tee ../kcl_output.log) || [ $? -eq 124 ]
 else
   echo "Unknown OS: $RUNNER_OS"
   exit 1
