1a7748400c58091787be174f6439259a3e852003
skye rogers <skyero@amazon.com>
2025-12-05 10:16:31 -0800

    refactor variables and file names and remove streamArn

--- .github/scripts/manipulate_properties.sh
+++ .github/scripts/manipulate_properties.sh
@@ -4,19 +4,19 @@ set -e
 # Manipulate sample.properties file that the KCL application pulls properties from (ex: streamName, applicationName)
 # Depending on the OS, different properties need to be changed
 if [[ "$RUNNER_OS" == "macOS" ]]; then
-  sed -i "" "s/kclrbsample/$STREAM_NAME/g" .github/resources/sample.properties
-  sed -i "" "s/RubyKCLSample/$APP_NAME/g" .github/resources/sample.properties
+  sed -i "" "s/STREAM_NAME_PLACEHOLDER/$STREAM_NAME/g" .github/resources/github_workflow.properties
+  sed -i "" "s/APP_NAME_PLACEHOLDER/$APP_NAME/g" .github/resources/github_workflow.properties
 elif [[ "$RUNNER_OS" == "Linux" || "$RUNNER_OS" == "Windows" ]]; then
-  sed -i "s/kclrbsample/$STREAM_NAME/g" .github/resources/sample.properties
-  sed -i "s/RubyKCLSample/$APP_NAME/g" .github/resources/sample.properties
+  sed -i "s/STREAM_NAME_PLACEHOLDER/$STREAM_NAME/g" .github/resources/github_workflow.properties
+  sed -i "s/APP_NAME_PLACEHOLDER/$APP_NAME/g" .github/resources/github_workflow.properties
   if [[ "$RUNNER_OS" == "Windows" ]]; then
     echo '@echo off' > samples/run_script.bat
     echo 'ruby %~dp0sample_kcl.rb %*' >> samples/run_script.bat
-    sed -i 's/executableName = sample_kcl.rb/executableName = run_script.bat/' .github/resources/sample.properties
+    sed -i 's/executableName = sample_kcl.rb/executableName = run_script.bat/' .github/resources/github_workflow.properties
   fi
 else
   echo "Unknown OS: $RUNNER_OS"
   exit 1
 fi
 
-cat .github/resources/sample.properties
+cat .github/resources/github_workflow.properties
--- .github/scripts/start_kcl.sh
+++ .github/scripts/start_kcl.sh
@@ -6,9 +6,9 @@ chmod +x samples/sample_kcl.rb
 
 if [[ "$RUNNER_OS" == "macOS" ]]; then
   brew install coreutils
-  (cd samples && gtimeout $RUN_TIME_SECONDS rake run properties_file=../.github/resources/sample.properties 2>&1 | tee ../kcl_output.log) || [ $? -eq 124 ]
+  (cd samples && gtimeout $RUN_TIME_SECONDS rake run properties_file=../.github/resources/github_workflow.properties 2>&1 | tee ../kcl_output.log) || [ $? -eq 124 ]
 elif [[ "$RUNNER_OS" == "Linux" || "$RUNNER_OS" == "Windows" ]]; then
-  (cd samples && timeout $RUN_TIME_SECONDS rake run properties_file=../.github/resources/sample.properties 2>&1 | tee ../kcl_output.log) || [ $? -eq 124 ]
+  (cd samples && timeout $RUN_TIME_SECONDS rake run properties_file=../.github/resources/github_workflow.properties 2>&1 | tee ../kcl_output.log) || [ $? -eq 124 ]
 else
   echo "Unknown OS: $RUNNER_OS"
   exit 1
