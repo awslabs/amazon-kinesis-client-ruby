89f843d968a5f1e516f4a82f65b40ae9cd2766e9
skyero-aws <skyero@amazon.com>
2025-12-08 16:10:48 -0800

    Add github workflow for KCL sample run testing (#124)

--- .github/scripts/manipulate_properties.sh
+++ .github/scripts/manipulate_properties.sh
@@ -4,29 +4,19 @@ set -e
 # Manipulate sample.properties file that the KCL application pulls properties from (ex: streamName, applicationName)
 # Depending on the OS, different properties need to be changed
 if [[ "$RUNNER_OS" == "macOS" ]]; then
-  sed -i "" "s/kclrbsample/$STREAM_NAME/g" samples/sample.properties
-  sed -i "" "s/RubyKCLSample/$APP_NAME/g" samples/sample.properties
-  sed -i "" 's/us-east-5/us-east-1/g' samples/sample.properties
-  grep -v "idleTimeBetweenReadsInMillis" samples/sample.properties > samples/temp.properties
-  echo "idleTimeBetweenReadsInMillis = 250" >> samples/temp.properties
-  mv samples/temp.properties samples/sample.properties
-elif [[ "$RUNNER_OS" == "Linux" ]]; then
-  sed -i "s/kclrbsample/$STREAM_NAME/g" samples/sample.properties
-  sed -i "s/RubyKCLSample/$APP_NAME/g" samples/sample.properties
-  sed -i 's/us-east-5/us-east-1/g' samples/sample.properties
-  sed -i "/idleTimeBetweenReadsInMillis/c\idleTimeBetweenReadsInMillis = 250" samples/sample.properties
-elif [[ "$RUNNER_OS" == "Windows" ]]; then
-  sed -i "s/kclrbsample/$STREAM_NAME/g" samples/sample.properties
-  sed -i "s/RubyKCLSample/$APP_NAME/g" samples/sample.properties
-  sed -i 's/us-east-5/us-east-1/g' samples/sample.properties
-  sed -i "/idleTimeBetweenReadsInMillis/c\idleTimeBetweenReadsInMillis = 250" samples/sample.properties
-
-  echo '@echo off' > samples/run_script.bat
-  echo 'ruby %~dp0\sample_kcl.rb %*' >> samples/run_script.bat
-  sed -i 's/executableName = sample_kcl.rb/executableName = run_script.bat/' samples/sample.properties
+  sed -i "" "s/STREAM_NAME_PLACEHOLDER/$STREAM_NAME/g" .github/resources/github_workflow.properties
+  sed -i "" "s/APP_NAME_PLACEHOLDER/$APP_NAME/g" .github/resources/github_workflow.properties
+elif [[ "$RUNNER_OS" == "Linux" || "$RUNNER_OS" == "Windows" ]]; then
+  sed -i "s/STREAM_NAME_PLACEHOLDER/$STREAM_NAME/g" .github/resources/github_workflow.properties
+  sed -i "s/APP_NAME_PLACEHOLDER/$APP_NAME/g" .github/resources/github_workflow.properties
+  if [[ "$RUNNER_OS" == "Windows" ]]; then
+    echo '@echo off' > samples/run_script.bat
+    echo 'ruby %~dp0sample_kcl.rb %*' >> samples/run_script.bat
+    sed -i 's/executableName = sample_kcl.rb/executableName = run_script.bat/' .github/resources/github_workflow.properties
+  fi
 else
   echo "Unknown OS: $RUNNER_OS"
   exit 1
 fi
 
-cat samples/sample.properties
+cat .github/resources/github_workflow.properties
